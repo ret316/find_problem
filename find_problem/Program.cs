b02f1c1f2dfa6f5b9aee71ebd3f6f6d101fc8457
ret316 <ret316@yandex.ru>
2019-10-25 12:32:52 +0400

    Добавьте файлы проекта.

---
+++ find_problem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace find_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
